88b533bbc111eab42dfbe36efe1cd08a0fe1b53b
Andres Gonzalez <miroenlared@gmail.com>
2025-03-20 12:16:17 -0500

    fix auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import bcrypt from 'bcryptjs';
 
 const handler = NextAuth({
   providers: [
